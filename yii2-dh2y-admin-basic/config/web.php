0abf4a1d45b4ef7c0213ca54d22891c15e788bb5
dailinlin <dailinlin@7477.com>
2017-07-24 19:05:23 +0800

    权限管理，节点方法添加bug

--- yii2-dh2y-admin-basic/config/web.php
+++ yii2-dh2y-admin-basic/config/web.php
@@ -10,7 +10,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => '08d8b299f21c4dd0b81190972e9233d9',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
